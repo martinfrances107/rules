0e7327bdd0ece9ce4ba36bc0c18da43719996184
Tim Rohaly <tr@202830.no-reply.drupal.org>
2019-02-15 12:51:50 -0800

    Issue #3033431 by TR: Allow RulesIntegrationTestBase to discover contributed modules

--- tests/src/Unit/Integration/RulesIntegrationTestBase.php
+++ tests/src/Unit/Integration/RulesIntegrationTestBase.php
@@ -11,6 +11,7 @@ use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityManagerInterface;
 use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\Discovery\RecursiveExtensionFilterIterator;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Path\AliasManagerInterface;
 use Drupal\Core\Plugin\Context\LazyContextRepository;
@@ -239,10 +240,10 @@ abstract class RulesIntegrationTestBase extends UnitTestCase {
   /**
    * Fakes the enabling of a module and adds its namespace for plugin loading.
    *
-   * Default behaviour works fine for core modules.
+   * This method allows plugins provided by a module to be discoverable.
    *
    * @param string $name
-   *   The name of the module that's gonna be enabled.
+   *   The name of the module that's going to be enabled.
    * @param array $namespaces
    *   Map of the association between module's namespaces and filesystem paths.
    */
@@ -250,10 +251,57 @@ abstract class RulesIntegrationTestBase extends UnitTestCase {
     $this->enabledModules[$name] = TRUE;
 
     if (empty($namespaces)) {
-      $namespaces = ['Drupal\\' . $name => $this->root . '/core/modules/' . $name . '/src'];
+      $namespaces = ['Drupal\\' . $name => $this->root . '/' . $this->constructModulePath($name) . '/src'];
     }
     foreach ($namespaces as $namespace => $path) {
       $this->namespaces[$namespace] = $path;
+    }
+  }
+
+  /**
+   * Determines the path to a module's class files.
+   *
+   * Core modules and contributed modules are located in different places, and
+   * the testbot (DrupalCI) does not use same directory structure as most live
+   * Drupal sites, so we must discover the path instead of hardwiring it.
+   *
+   * This method discovers modules the same way as Drupal core, so it should
+   * work for core and contributed modules in all environments.
+   *
+   * @see \Drupal\Core\Extension\ExtensionDiscovery
+   */
+  protected function constructModulePath($module) {
+    // Use Unix paths regardless of platform, skip dot directories, follow
+    // symlinks (to allow extensions to be linked from elsewhere), and return
+    // the RecursiveDirectoryIterator instance to have access to getSubPath(),
+    // since SplFileInfo does not support relative paths.
+    $flags = \FilesystemIterator::UNIX_PATHS;
+    $flags |= \FilesystemIterator::SKIP_DOTS;
+    $flags |= \FilesystemIterator::FOLLOW_SYMLINKS;
+    $flags |= \FilesystemIterator::CURRENT_AS_SELF;
+    $directory_iterator = new \RecursiveDirectoryIterator($this->root, $flags);
+
+    // Filter the recursive scan to discover extensions only.
+    // Important: Without a RecursiveFilterIterator, RecursiveDirectoryIterator
+    // would recurse into the entire filesystem directory tree without any kind
+    // of limitations.
+    $filter = new RecursiveExtensionFilterIterator($directory_iterator);
+    // Ensure we find testing modules too!
+    $filter->acceptTests(TRUE);
+
+    // The actual recursive filesystem scan is only invoked by instantiating the
+    // RecursiveIteratorIterator.
+    $iterator = new \RecursiveIteratorIterator($filter,
+      \RecursiveIteratorIterator::LEAVES_ONLY,
+      // Suppress filesystem errors in case a directory cannot be accessed.
+      \RecursiveIteratorIterator::CATCH_GET_CHILD
+
+    );
+
+    $info_files = new \RegexIterator($iterator, "/^$module.info.yml$/");
+    foreach ($info_files as $file) {
+      // There should only be one match.
+      return $file->getSubPath();
     }
   }
 
